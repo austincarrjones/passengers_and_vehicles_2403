808087fb08d1f2974e72347ae0abd53db044d8db
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-04-03 09:46:05 -0600

    test: create Vehicle tests

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,69 @@
+require 'pry'
+require './lib/vehicle'
+require './lib/passenger'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+  end
+  
+RSpec.describe Vehicle do
+  
+  it 'can initialize' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    expect(vehicle).to be_an_instance_of(Vehicle)
+  end
+
+  it 'has a year' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    expect(vehicle.year).to eq("2001")
+  end
+
+  it 'has a make' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    expect(vehicle.make).to eq("Honda")
+  end
+  
+  it 'has a make' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    expect(vehicle.model).to eq("Civic")
+  end
+
+  it 'is not speeding' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    expect(vehicle.speeding?).to be false
+
+  it 'is speeding' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    vehile.speed
+    expect(vehicle.speeding?).to be true
+  end
+
+  it 'has no passengers' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    expect(vehicle.passengers).to eq[]
+  end
+
+  it 'can add passengers' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+    jude = Passenger.new({"name" => "Jude", "age" => 20})    
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
+    vehicle.add_passenger(charlie)
+    vehicle.add_passenger(jude)
+    vehicle.add_passenger(taylor)
+    expect(vehicle.passengers).to eq[charlie, jude, taylor]
+  end
+
+  it 'can count adult passengers' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+    jude = Passenger.new({"name" => "Jude", "age" => 20})    
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
+    vehicle.add_passenger(charlie)
+    vehicle.add_passenger(jude)
+    vehicle.add_passenger(taylor)
+    expect(vehicle.passengers).to eq[charlie, jude, taylor]
+    expect(vehicle.num_adults).to eq(2)
+  end
+
+end
