b32bd18264ea135eedbad39f378abcd674f57307
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-04-03 09:34:38 -0600

    feature: Passenger code and pass all tests

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -13,28 +13,36 @@ RSpec.describe Passenger do
   end
 
   it 'can accept a different passenger' do #I feel like this could be combined with above somehow.
-    expect (taylor = Passenger.new({"name" => "Taylor", "age" => 12})).to be_an_instance_of(Passenger)
+    expect(taylor = Passenger.new({"name" => "Taylor", "age" => 12})).to be_an_instance_of(Passenger)
   end
 
   it 'has a name' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     expect(charlie.name).to eq("Charlie")
   end
 
   it 'has an age' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     expect(charlie.age).to eq(18)
   end
 
   it 'is an adult' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     expect(charlie.adult?).to be true
   end
 
   it 'is not an adult' do
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
     expect(taylor.adult?).to be false
+  end
 
   it 'is not a driver' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     expect(charlie.driver?).to be false
+  end
 
   it 'can make a driver' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     charlie.drive
     expect(charlie.driver?).to be true
   end 
